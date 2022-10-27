c9b45dc7db572c4103968c8283de1c05288ab4fd
sebastian <sebastian.waldschmidt-2@mni.thm.de>
2022-10-27 14:09:31 +0200

    Didnt do anything. just imported bootstrap-vue. Bootstrap because we dont need to think about styling

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+
+
+createApp(App).mount('#app');
+
