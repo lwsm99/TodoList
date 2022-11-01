3b04e1f2cf2f0cb913d951660578adf839e6aeea
shinymew2 <maxim.odenweller@mni.thm.de>
2022-11-01 18:35:12 +0100

    Delete with dialog

--- src/App.vue
+++ src/App.vue
@@ -93,6 +93,28 @@
     </div>
   </div>
 
+<!-- Modal Delete Task -->
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Löschen</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Wirklich diese Aufgabe löschen?
+        <br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" @click="deleteTask(task)" data-dismiss="modal">Löschen</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <table class="table">
   <thead>
     <tr>
@@ -121,7 +143,7 @@
         </button>
       </td>
       <td>
-        <button type="button" class="btn btn-primary" @click="deleteTask(task)">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#deleteModal">
           Löschen
         </button>
       </td>
