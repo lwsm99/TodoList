72e5cba963d40242ea060e5a7d8703f396aa7ee2
sebastian <sebastian.waldschmidt-2@mni.thm.de>
2022-11-01 17:12:03 +0100

    Dunno

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <div id="app">
+
+
+
     <h1 style="text-align: center">Todo-List</h1>
 
       <div class="form-group">
         <input type="text" v-model="addTaskInput" class="form-control" style="width: 20rem">
       </div>
+
+    <select class="form-select form-select-lg mb-3" style="width: 20rem" >
+      <option selected disabled>Prio</option>
+    </select>
+
+    <div class="form-group">
+      <input type="text" v-model="addKategorieInput" class="form-control" style="width: 20rem">
+    </div>
+
+    <button class="btn btn-secondary" @click="addKategorie">Kategorie hinzufügen</button>
+
+
+    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" style="width: 20rem" v-model="addTaskInputPrio">
+      <option selected disabled>Prio</option>
+      <option value="3">Hoch</option>
+      <option value="2">Mittel</option>
+      <option value="1">Niedrig</option>
+    </select>
+    <br>
+
       <button @click="addToList" class="btn btn-primary">add to List</button>
 
     <div v-for="task in taskArray" :key="task.id">
@@ -29,16 +52,23 @@ export default {
       taskArray: [
 
       ],
+      kategorieArray:[],
       addTaskInput:"",
+      addTaskInputPrio:0,
+
+      addKategorieInput:"",
     }
   },
   methods:{
     addToList(){
       this.taskArray.push(
-          {freitext: this.addTaskInput, prio: 1, done: false}
-
+          {freitext: this.addTaskInput, prio: this.addTaskInputPrio, done: false}
       )
       this.addTaskInput = '';
+      this.addTaskInputPrio = '';
+    },
+    addKategorie(){
+      this.kategorieArray.push(this.addKategorieInput)
     }
   }
 }
