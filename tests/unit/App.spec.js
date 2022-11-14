41edf7fb7497eddf6ddbf49c369027fd506cc099
Maxim <maxim.odenweller@mni.thm.de>
2022-11-14 15:19:58 +0100

    Add unit tests

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,6 +1,30 @@
 import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
+/*
+  Helper Methods
+*/
+
+function registerUsers(usersToRegister) {
+  const wrapper = shallowMount(App)
+  for(let i = 0; i < usersToRegister; i++) {
+    wrapper.vm.addUser();
+  }
+  return wrapper;
+}
+
+function deleteUsers(givenUsers, usersToDelete) {
+  const wrapper = givenUsers;
+  for(let i = 0; i < usersToDelete; i++) {
+    wrapper.vm.removeObjectWithId(wrapper.vm.userArray, i);
+  }
+  return wrapper;
+}
+
+/* 
+  Tests
+*/
+
 describe('App.vue', () => {
   it('adds item to list', () => {
     const wrapper = shallowMount(App)
@@ -9,4 +33,16 @@ describe('App.vue', () => {
 
     expect(wrapper.vm.taskArray).toHaveLength(1)
   })
-})
+});
+
+describe('User', () => {
+  test('register users', () => {
+    const wrapper = registerUsers(15);
+    expect(wrapper.vm.userArray).toHaveLength(15)
+  });
+
+  test('delete users', () => {
+    const wrapper = deleteUsers(registerUsers(15), 4);
+    expect(wrapper.vm.userArray).toHaveLength(11);
+  });
+});
